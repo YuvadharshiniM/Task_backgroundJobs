32d6fdabc34746e6a72d76253e506a0a8f53029e
YuvadharshiniM <yuvadharshinim05@gmail.com>
2026-09-15 08:45:29 +0000

    Last Task

---
+++ sample_app/www/articles.py
@@ -0,0 +1,12 @@
+import frappe
+
+
+def get_context(context):
+    context.articles = frappe.get_all(
+        "Article",
+        filters={"status": "Published"},
+        fields=["title", "name"]
+    )
+
+    return context
+
